676ff2a14f22b251fe457e42de8039d8445c4865
Abdallah <abdallaahnoor@gmail.com>
2024-10-02 14:34:39 +0300

    update

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -25,7 +25,7 @@ router.route("/").post(async (req, res) => {
       // quality: "hd",
       prompt,
       n: 1,
-      size: "1024x1024",
+      size: "256x256",
       response_format: "b64_json",
     });
 
